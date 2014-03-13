e692726cd061df66d24950ed2c6d4ef4984b9484
chadcolby <chad.david.colby@gmail.com>
2014-03-13 13:41:04 -0700

    More Google API

---
+++ Lux Coffee/CCCalendarEvents.h
@@ -0,0 +1,17 @@
+//
+//  CCCalendarEvents.h
+//  Lux Coffee
+//
+//  Created by Chad D Colby on 3/13/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCCalendarEvents : NSObject
+
+@property (strong, nonatomic) NSString *titleString;
+@property (strong, nonatomic) NSString *contentString;
+@property (strong, nonatomic) NSString *locationString;
+
+@end
---
+++ Lux Coffee/CCCalendarEvents.m
@@ -0,0 +1,15 @@
+//
+//  CCCalendarEvents.m
+//  Lux Coffee
+//
+//  Created by Chad D Colby on 3/13/14.
+//  Copyright (c) 2014 Byte Meets World. All rights reserved.
+//
+
+#import "CCCalendarEvents.h"
+
+@implementation CCCalendarEvents
+
+
+
+@end
